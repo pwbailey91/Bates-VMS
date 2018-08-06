c65b9987637edeba7e7b3d37553d951ae13e1535
pbailey <peterwbailey91@gmail.com>
2018-08-06 14:22:02 -0400

    Use pledge_install instead of pledge_credit

--- VMS_giftsummary.sql
+++ VMS_giftsummary.sql
@@ -36,22 +36,23 @@ where (con.primary_donor_code='A'
 group by con.cons_id, cr.fiscal_year
 union all
 select con.cons_id                                        as "Constituent_Externalid",
-       sum(pcr.credit_amount-pcr.credit_amount_paid)      as "GiftSummary_Amount",
+       sum(pin.expected_amt-pin.install_amt_paid)         as "GiftSummary_Amount",
        'Pledge Balance'                                   as "GiftSummary_Type",
-       pcr.fiscal_year                                    as "GiftSummary_Year"
+       pin.install_fiscal_year                            as "GiftSummary_Year"
 from adv_constituent_d con
-     inner join adv_pledge_credit_f pcr on con.household_key=pcr.household_key
-     inner join adv_pldg_description_d pld on pcr.pledge_description_key=pld.pldg_description_key
+     inner join adv_pledge_install_f pin on con.constituent_key=pin.constituent_key_pledger
+     inner join adv_pldg_description_d pld on pin.pledge_description_key=pld.pldg_description_key
      inner join adv_reportvars_d rv on rv.var_name='FY_RPT'
-     inner join adv_campaign_d cam on pcr.campaign_key=cam.campaign_key
+     inner join adv_campaign_d cam on pin.campaign_key=cam.campaign_key
      left outer join last_gift on con.household_key=last_gift.household_key
      left outer join trustee_giving on con.constituent_key=trustee_giving.constituent_key
 where (con.primary_donor_code='A' 
       or (con.primary_donor_code='P' and ((case con.parent_scy when 'n/a' then '0' else con.parent_scy end)>=rv.var_value-3 or last_gift.fiscal_year >= rv.var_value-1)))
       and pld.soft_credit_ind='N'
       and pld.anon_ind='N'
-      and pcr.fiscal_year between rv.var_value-5 and rv.var_value
+      --and pld.pledge_status_sd='A'
+      and pin.install_fiscal_year between rv.var_value-5 and rv.var_value
       and (trustee_giving.constituent_key is null or (cam.campaign_type_sd='AF' and cam.campaign_sd like 'B_F%'))--Only BF for trustees
-group by con.cons_id, pcr.fiscal_year
-having sum(pcr.credit_amount-pcr.credit_amount_paid)>0
+group by con.cons_id, pin.install_fiscal_year
+having sum(pin.expected_amt-pin.install_amt_paid)>0
 order by "Constituent_Externalid", "GiftSummary_Year", "GiftSummary_Type"
