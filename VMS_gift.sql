f6dd43362903a3d619f70bdd83afd2b7c8ab2e44
pbailey <peterwbailey91@gmail.com>
2018-09-19 10:12:42 -0400

    Revert "Change gift file to individual giving"
    This reverts commit b263cf289c20ab8103ed6742c4ded4a5baa6a662.

--- VMS_gift.sql
+++ VMS_gift.sql
@@ -17,22 +17,22 @@ from adv_constituent_d con
 where dc.donor_code_sd in ('T','TS')
       or afr.AFRCTYP_SOL_ORG='TP'
 )
-select con.cons_id                                                                                                      as "Constituent_Externalid",
-       cr.credit_amount                                                                                                 as "Gift_Amount",
-       'Cash In'                                                                                                        as "Gift_Type",
-       des.designation_ld                                                                                               as "Gift_Allocation",
-       cr.fiscal_year                                                                                                   as "Gift_Year",
-       cr.gift_number||to_char(row_number() over 
-       (partition by cr.gift_number 
-                  order by con.constituent_key,cr.date_key_gift,cr.campaign_key,
-                        cr.designation_key,cr.gift_description_key,cr.pledge_number),'FM09')                            as "Gift_TransactionId",
-       to_char(cal.calendar_date,'MM/DD/YYYY')                                                                          as "Gift_Date"
+select con.cons_id                                                                                                        as "Constituent_Externalid",
+       hhg.credit_amount                                                                                                  as "Gift_Amount",
+       'Cash In'                                                                                                          as "Gift_Type",
+       des.designation_ld                                                                                                 as "Gift_Allocation",
+       hhg.fiscal_year                                                                                                    as "Gift_Year",
+       hhg.gift_number||to_char(row_number() over 
+       (partition by hhg.gift_number 
+                  order by con.constituent_key,hhg.date_key_gift,hhg.campaign_key,
+                        hhg.designation_key,hhg.gift_description_key,hhg.pledge_number),'FM09')                           as "Gift_TransactionId",
+       to_char(cal.calendar_date,'MM/DD/YYYY')                                                                            as "Gift_Date"
 from adv_constituent_d con
-     inner join adv_credit_f cr on con.constituent_key=cr.constituent_key_credit
-     inner join adv_gift_description_d gd on cr.gift_description_key=gd.gift_description_key
-     inner join adv_designation_d des on cr.designation_key=des.designation_key
-     inner join adv_campaign_d cam on cr.campaign_key=cam.campaign_key
-     inner join adv_calendar_dv cal on cr.date_key_gift=cal.date_key
+     inner join adv_hh_giving_f hhg on con.household_key=hhg.household_key
+     inner join adv_gift_description_d gd on hhg.gift_description_key=gd.gift_description_key
+     inner join adv_designation_d des on hhg.designation_key=des.designation_key
+     inner join adv_campaign_d cam on hhg.campaign_key=cam.campaign_key
+     inner join adv_calendar_dv cal on hhg.date_key_gift=cal.date_key
      inner join adv_reportvars_d rv on rv.var_name='VOLUNTR_FY'
      left outer join last_gift on con.household_key=last_gift.household_key
      left outer join trustee_giving on con.constituent_key=trustee_giving.constituent_key
@@ -40,5 +40,5 @@ where ((con.primary_donor_code='A' and con.scy>=to_char(rv.var_value-70))
       or (con.primary_donor_code='P' and (replace(con.parent_scy,'n/a','0')>=rv.var_value-3 or last_gift.fiscal_year >= rv.var_value-1)))
       and gd.soft_credit_ind='N'
       and gd.anon_ind='N'
-      and cr.fiscal_year between rv.var_value-5 and rv.var_value
+      and hhg.fiscal_year between rv.var_value-5 and rv.var_value
       and (trustee_giving.constituent_key is null or cam.campaign_type_sd='AF')--Only BF for trustees
