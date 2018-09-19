6e64cb430b2c338b5f850f8701ab9ff10f5d0ad4
pbailey <peterwbailey91@gmail.com>
2018-09-19 09:56:34 -0400

    Revert "Use individual giving for last gifts"
    This reverts commit 398c5121234a410f579ec27e814fcbc42b2be860.

--- VMS_gift.sql
+++ VMS_gift.sql
@@ -1,11 +1,11 @@
 /*Query to generate the gift file for the VMS.*/
 
-with last_gift as (--Get fiscal year of most recent gift, used in filtering parents to include
-select cr.constituent_key_credit, max(cr.fiscal_year) as fiscal_year
-from adv_credit_f cr
-     inner join adv_gift_description_d gd on cr.gift_description_key=gd.gift_description_key
+with last_gift as (--Get fiscal year of most recent household gift, used in filtering parents to include
+select hhg.household_key, max(hhg.fiscal_year) as fiscal_year
+from adv_hh_giving_f hhg
+     inner join adv_gift_description_d gd on hhg.gift_description_key=gd.gift_description_key
 where gd.soft_credit_ind='N' and gd.anon_ind='N'
-group by cr.constituent_key_credit
+group by hhg.household_key
 ),
 trustee_giving as (--Only include BF giving for trustees and top prospects
 select distinct con.constituent_key
@@ -34,7 +34,7 @@ from adv_constituent_d con
      inner join adv_campaign_d cam on cr.campaign_key=cam.campaign_key
      inner join adv_calendar_dv cal on cr.date_key_gift=cal.date_key
      inner join adv_reportvars_d rv on rv.var_name='VOLUNTR_FY'
-     left outer join last_gift on con.constituent_key=last_gift.constituent_key_credit
+     left outer join last_gift on con.household_key=last_gift.household_key
      left outer join trustee_giving on con.constituent_key=trustee_giving.constituent_key
 where ((con.primary_donor_code='A' and con.scy>=to_char(rv.var_value-70))
       or (con.primary_donor_code='P' and (replace(con.parent_scy,'n/a','0')>=rv.var_value-3 or last_gift.fiscal_year >= rv.var_value-1)))
--- VMS_relative.sql
+++ VMS_relative.sql
@@ -1,10 +1,10 @@
 /*Query to generate the relatives file for the VMS. Gets all children and spouses that went to Bates.*/
-with last_gift as (--Get fiscal year of most recent gift, used in filtering parents to include
-select cr.constituent_key_credit, max(cr.fiscal_year) as fiscal_year
-from adv_credit_f cr
-     inner join adv_gift_description_d gd on cr.gift_description_key=gd.gift_description_key
+with last_gift as (--Get fiscal year of most recent household gift, used in filtering parents to include
+select hhg.household_key, max(hhg.fiscal_year) as fiscal_year
+from adv_hh_giving_f hhg
+     inner join adv_gift_description_d gd on hhg.gift_description_key=gd.gift_description_key
 where gd.soft_credit_ind='N' and gd.anon_ind='N'
-group by cr.constituent_key_credit
+group by hhg.household_key
 )
 select con.cons_id                                                                  as "Constituent_Externalid",
        rel.cons_id                                                                  as "Relative_ExternalId",
@@ -22,7 +22,7 @@ from adv_constituent_d con
 inner join aprxref xref on con.pidm=xref.aprxref_pidm
 inner join adv_constituent_d rel on xref.aprxref_xref_pidm=rel.pidm
 inner join adv_reportvars_d rv on var_name='VOLUNTR_FY'
-left outer join last_gift on con.constituent_key=last_gift.constituent_key_credit
+left outer join last_gift on con.household_key=last_gift.household_key
 where xref.aprxref_xref_code in ('SPS','CHL','SCH','WRD','PRT')
 and ((con.primary_donor_code='A' and con.scy>=to_char(rv.var_value-70))
       or (con.primary_donor_code='P' and (replace(con.parent_scy,'n/a','0')>=rv.var_value-3 or last_gift.fiscal_year >= rv.var_value-1)))
