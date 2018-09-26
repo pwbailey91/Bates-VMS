5bc4a60cb6e2131c9050873d50cc5dcf23821909
pbailey <peterwbailey91@gmail.com>
2018-09-26 16:06:04 -0400

    Revert "Include class year in relative name"
    This reverts commit 152c4b311711dfb337a5f94398f3f53df28643e7.

--- VMS_relative.sql
+++ VMS_relative.sql
@@ -1,16 +1,16 @@
 /*Query to generate the relatives file for the VMS. Gets all children and spouses that went to Bates.*/
 
-select con.cons_id                                                                  as "Constituent_Externalid",
-       rel.cons_id                                                                  as "Relative_ExternalId",
-       rel.first_name || ' ' || rel.last_name || ' ''' || substr(rel.scy,3,2)       as "Relative_Name",
-       rel.scy                                                                      as "Relative_ClassYear",
+select con.cons_id                                                  as "Constituent_Externalid",
+       rel.cons_id                                                  as "Relative_ExternalId",
+       rel.first_name || ' ' || rel.last_name                       as "Relative_Name",
+       rel.scy                                                      as "Relative_ClassYear",
        case xref.aprxref_xref_code when 'SPS' then 'Spouse'
                                    when 'CHL' then 'Child'
                                    when 'SCH' then 'Stepchild'
                                    when 'WRD' then 'Ward'
-                                   when 'PRT' then 'Partner' end                    as "Relative_Type",
-       rel.gender                                                                   as "Relative_Gender",
-       case rel.deceased_ind when 'Y' then 'TRUE' else 'FALSE' end                  as "Relative_Deceased"
+                                   when 'PRT' then 'Partner' end    as "Relative_Type",
+       rel.gender                                                   as "Relative_Gender",
+       case rel.deceased_ind when 'Y' then 'TRUE' else 'FALSE' end  as "Relative_Deceased"
 from adv_constituent_d con
      inner join aprxref xref on con.pidm=xref.aprxref_pidm
      inner join adv_constituent_d rel on xref.aprxref_xref_pidm=rel.pidm
