99b476fe1357388ffa781bd60e7c89ae68df1c20
pbailey <peterwbailey91@gmail.com>
2018-09-27 10:52:24 -0400

    Change gift year to campaign fiscal year

--- VMS_gift.sql
+++ VMS_gift.sql
@@ -4,7 +4,7 @@ select con.cons_id                                                              
        hhg.credit_amount                                                                                                  as "Gift_Amount",
        'Cash In'                                                                                                          as "Gift_Type",
        des.designation_ld                                                                                                 as "Gift_Allocation",
-       hhg.fiscal_year                                                                                                    as "Gift_Year",
+       cal2.fiscal_year                                                                                                   as "Gift_Year",
        hhg.gift_number||to_char(row_number() over 
        (partition by hhg.gift_number 
                   order by con.constituent_key,hhg.date_key_gift,hhg.campaign_key,
@@ -16,9 +16,10 @@ from adv_constituent_d con
      inner join adv_designation_d des on hhg.designation_key=des.designation_key
      inner join adv_campaign_d cam on hhg.campaign_key=cam.campaign_key
      inner join adv_calendar_dv cal on hhg.date_key_gift=cal.date_key
+     inner join adv_calendar_dv cal2 on cam.date_key_est=cal2.date_key
      inner join adv_reportvars_d rv on rv.var_name='VOLUNTR_FY'
 where (con.primary_donor_code='A' and con.scy>=to_char(rv.var_value-70))
       and gd.soft_credit_ind='N'
       and gd.anon_ind='N'
-      and hhg.fiscal_year between rv.var_value-5 and rv.var_value
+      and cal2.fiscal_year between rv.var_value-5 and rv.var_value
       and cam.campaign_type_sd='AF' --Only BF gifts
--- VMS_giftsummary.sql
+++ VMS_giftsummary.sql
@@ -3,18 +3,19 @@
 select con.cons_id                                        as "Constituent_Externalid",
        sum(cr.credit_amount)                              as "GiftSummary_Amount",
        'Cash In - Summary'                                as "GiftSummary_Type",
-       cr.fiscal_year                                     as "GiftSummary_Year"
+       cal.fiscal_year                                    as "GiftSummary_Year"
 from adv_constituent_d con
      inner join adv_credit_f cr on con.constituent_key=cr.constituent_key_credit
      inner join adv_gift_description_d gd on cr.gift_description_key=gd.gift_description_key
      inner join adv_reportvars_d rv on rv.var_name='VOLUNTR_FY'
      inner join adv_campaign_d cam on cr.campaign_key=cam.campaign_key
+     inner join adv_calendar_dv cal on cam.date_key_est=cal.date_key
 where (con.primary_donor_code='A' and con.scy>=to_char(rv.var_value-70))
       and gd.soft_credit_ind='N'
       and gd.anon_ind='N'
-      and cr.fiscal_year between rv.var_value-5 and rv.var_value
+      and cal.fiscal_year between rv.var_value-5 and rv.var_value
       and cam.campaign_type_sd='AF' --Only BF gifts
-group by con.cons_id, cr.fiscal_year
+group by con.cons_id, cal.fiscal_year
 union all
 select "Constituent_Externalid",
        "GiftSummary_Amount",
@@ -28,21 +29,22 @@ select con.cons_id                                        as "Constituent_Extern
        (count(distinct sps.constituent_key)+1)            as "Pledge Balance - Summary",
        sum(pin.expected_amt)/
        (count(distinct sps.constituent_key)+1)            as "Pledge Amount - Summary",
-       pin.install_fiscal_year                            as "GiftSummary_Year"
+       cal.fiscal_year                                    as "GiftSummary_Year"
 from adv_constituent_d con
      inner join adv_pledge_install_f pin on con.constituent_key=pin.constituent_key_pledger
      inner join adv_pldg_description_d pld on pin.pledge_description_key=pld.pldg_description_key
      inner join adv_reportvars_d rv on rv.var_name='VOLUNTR_FY'
      inner join adv_campaign_d cam on pin.campaign_key=cam.campaign_key
+     inner join adv_calendar_dv cal on cam.date_key_est=cal.date_key
      inner join adv_household_b hhb on con.household_key=hhb.household_key and con.constituent_key=hhb.cons_key_sps1
      left outer join adv_constituent_d sps on hhb.cons_key_sps2=sps.constituent_key and sps.primary_donor_code='A'
 where (con.primary_donor_code='A' and con.scy>=to_char(rv.var_value-70))
       and pld.soft_credit_ind='N'
       and pld.anon_ind='N'
       and pld.pledge_status_sd='A'
-      and pin.install_fiscal_year=rv.var_value
+      and cal.fiscal_year=rv.var_value
       and cam.campaign_type_sd='AF' --Only BF pledges
-group by con.cons_id, pin.install_fiscal_year
+group by con.cons_id, cal.fiscal_year
 ) unpivot 
   ("GiftSummary_Amount" for "GiftSummary_Type" in ("Pledge Balance","Pledge Amount","Pledge Balance - Summary","Pledge Amount - Summary")
 )
